ea34e35a33c93d729c61c73b75fbc3df3dcfc20e
René Rudat <rene_rudat@creati-work.ch>
2025-05-24 14:34:20 +0200

    changed semicolons

--- m294-game/src/App.jsx
+++ m294-game/src/App.jsx
@@ -1,18 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+//Dummydaten für die erste Frage
+const question1 = {
+  question: "Welcher Begriff kommt im ICAO-Buchstabieralphabet zuerst?",
+  answers: ["Alfa", "Bravo", "Charlie"],
+  correctAnswer: "Alfa" // Brauchen wir heute noch nicht, gut für später
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-
       <h1>Willkommen beim WISS-Quiz</h1>
       <button>Nächste Frage</button>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
