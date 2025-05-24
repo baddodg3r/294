e1d2231ba2cee0a5128b8d32066f322d8b7d487d
René Rudat <rene_rudat@creati-work.ch>
2025-05-24 15:44:18 +0200

    FINISH 4A

--- m294-game/src/App.jsx
+++ m294-game/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import Button from './components/button';
 
 //Dummydaten für die erste Frage
 const question1 = {
@@ -10,14 +11,36 @@ const question1 = {
   correctAnswer: "Alfa" // Brauchen wir heute noch nicht, gut für später
 };
 
+
+
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <h1>Willkommen beim WISS-Quiz!</h1>
-      <h2>{question1.question}</h2>
-      {/* Hier kommen gleich das Bild und die Buttons hin */}
+      <header>
+        <h1>Willkommen beim WISS-Quiz!</h1>
+        <h2>{question1.question}</h2>
+      </header>
+
+      <div className='buttonbar'>
+        {question1.answers.map((answer) => (
+          <Button
+
+            key={answer}
+            text={answer}
+
+            onAnswerClick={
+              () => {
+                console.log(`Antwort ${answer} wurde geclickt`);
+              }
+            }
+
+          >
+          </Button>
+
+        ))}
+
+      </div>
     </div>
   );
 };
